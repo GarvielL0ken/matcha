da1bda3a84cc65bf9f9f74889819138a9d043694
GarvielL0ken <jsarkis@student.wethinkcode.co.za>
2020-01-23 14:31:47 +0200

    added inputs for a new user to input their data

--- site/registration.php
+++ site/registration.php
@@ -2,3 +2,14 @@
     $page = "Registration";
     require_once "header.php";
 ?>
+<html>
+    <form action= '../config/new_user.php' method= "post">
+        <pre>First Name:       <input required type= "text" placeholder= "John" name= "first_name"></pre>
+        <pre>Last name:        <input required type= "text" placeholder= "Doe" name= "last_name"></pre>
+        <pre>Username:         <input required type= "text" placeholder= "J.D0e" name= "username"></pre>
+        <pre>Email Address:    <input required type= "text" placeholder= "JDoe@gmail.com" name= "email"></pre>
+        <pre>Password:         <input required type= "password" name= "passwd"></pre>
+        <pre>Confirm password: <input required type= "password" name= "confirm_passwd"></pre>
+        <input type= "submit" name= "submit" value= "Submit">
+    </form>
+</html>
