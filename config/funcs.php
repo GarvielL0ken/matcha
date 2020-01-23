850a6b1582347faed3d5c9eaa146e0b10dde913b
GarvielL0ken <jsarkis@student.wethinkcode.co.za>
2020-01-23 13:03:36 +0200

    working on header.php

--- config/funcs.php
+++ config/funcs.php
@@ -3,4 +3,10 @@
     {
         header('Location: ' . $page);
     }
+
+    function output_a($href, $string)
+    {
+        $html = '<a href= "' . $href . '">' . $string . '</a>';
+        return ($html);
+    }
 ?>
--- site/header.php
+++ site/header.php
@@ -11,5 +11,15 @@
     </head>
         <body>
             <h1><?php print($page)?></h1>
+            <?php
+                $p = array('Chat' => 0, 'Login' => 0, 'Notifications' => 0, 'Profile' => 0, 'Registration' => 0, 'Reset Password' => 0, 'Search' => 0, 'Suggestions' => 0, 'View' => 0);
+                $p[$page] = 1;
+                if ($p['Login'])
+                    print(output_a('registration.php', 'Register'));
+                if ($p['Registration'])
+                    print (output_a('login.php', 'Login'));
+                if (!$p['Login'] && !$p['Registration'] && !$p['Reset Password'])
+                    print(output_a('logout.php', 'Logout'));
+            ?>
         </body>
 </html>
