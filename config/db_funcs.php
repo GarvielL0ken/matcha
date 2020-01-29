3ce50b511e444b661485e88d68a4b53dec85f6d7
GarvielL0ken <jsarkis@student.wethinkcode.co.za>
2020-01-29 12:22:28 +0200

    working on connection to db and user creation

--- config/db_funcs.php
+++ config/db_funcs.php
@@ -1,2 +1,21 @@
 <?php
+    function insert_new_record($table, $data)
+    {
+        $conn = connect_to_db();
+        $keys = array_keys($data);
+        print_r($keys);
+        $stmt = $conn->prepare('INSERT INTO ' . $table . ' (' . $fields . ') VALUES (' . $values . ')');
+        $stmt->execute($data);
+    }
+
+    function is_in_db($table, $column, $value)
+    {
+        $conn = connect_to_db();
+        $stmt = $conn->prepare('SELECT ' . $column . ' FROM ' . $table . ' WHERE ' . $column . ' = :value');
+        $stmt->execute(array('value' => $value));
+        $results = $stmt->fetchAll();
+        if (!$results)
+            return (0);
+        return (1);
+    }
 ?>
